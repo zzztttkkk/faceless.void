06c96cc48539ad7d3b1e6101f5f8f5274d3488b9
zzztttkkk <ztkisalreadytaken@gmail.com>
2024-08-07 18:32:52 +0800

    [🧹 Chore] (/): -

--- gin.go
+++ gin.go
@@ -2,8 +2,8 @@ package fv
 
 import (
 	"context"
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"reflect"
 )
 
@@ -44,19 +44,88 @@ func (g *HttpGroup) Register(methods string, path string, fnc any) *HttpGroup {
 	if ft.NumIn() < 1 || ft.In(0) != ctxInterfaceType {
 		panic("fv")
 	}
-	if ft.NumOut() < 1 || ft.Out(0) != ctxInterfaceType {
+	if ft.NumOut() != 1 {
 		panic("fv")
 	}
-	// todo get doc info
+
+	argTypes := make([]reflect.Type, 0)
+	for i := 1; i < ft.NumIn(); i++ {
+		at := ft.In(i)
+		if at.Kind() != reflect.Ptr {
+			panic("fv")
+		}
+		at = at.Elem()
+		if at.Kind() != reflect.Struct {
+			panic("fv")
+		}
+		argTypes = append(argTypes, at)
+	}
 
 	handleFnc := func(c *gin.Context) {
-		// todo bind args
 		args := make([]reflect.Value, ft.NumIn())
 		args[0] = reflect.ValueOf(c)
-		outs := fv.Call(args)
-		fmt.Println(outs)
-		// todo write response
+		for idx, at := range argTypes {
+			av := reflect.New(at)
+			if e := c.Bind(av.Interface()); e != nil {
+				panic(e)
+			}
+			args[idx+1] = av
+		}
+		out := fv.Call(args)[0]
+		ginRespAny(c, out)
 	}
 	g.fncs = append(g.fncs, httpFunc{Methods: methods, Path: path, Fnc: handleFnc})
 	return g
+}
+
+type STRING string
+type HTML string
+type FILE string
+type STATUS_CODE int
+
+func ginRespAny(ctx *gin.Context, v reflect.Value) {
+	vt := v.Type()
+	if vt.Kind() == reflect.Ptr {
+		vt = vt.Elem()
+		v = v.Elem()
+	}
+	switch vt.Kind() {
+	case reflect.Func, reflect.Pointer, reflect.Chan:
+		{
+			panic("")
+		}
+	default:
+		{
+		}
+	}
+
+	iv := v.Interface()
+
+	switch tv := iv.(type) {
+	case FILE:
+		{
+			ctx.File((string)(tv))
+			return
+		}
+	case HTML:
+		{
+			ctx.HTML(http.StatusOK, (string)(tv), nil)
+			return
+		}
+	case STRING:
+		{
+			ctx.String(http.StatusOK, (string)(tv))
+			return
+		}
+	case STATUS_CODE:
+		{
+			ctx.Status(int(tv))
+			return
+		}
+	default:
+		{
+			ctx.JSON(http.StatusOK, tv)
+			return
+		}
+	}
 }
